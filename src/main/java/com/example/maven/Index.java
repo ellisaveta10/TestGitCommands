1b30a0707c628848751badaa7c2df8457b14bac5
vasilevael <Elisaveta.Vasileva@mail.schwarz>
2022-08-07 09:51:29 +0300

    Reabsing master

--- src/main/java/com/example/maven/Index.java
+++ src/main/java/com/example/maven/Index.java
@@ -1,9 +1,13 @@
 package com.example.maven;
 
+import java.time.Instant;
+
 public class Index {
     private String id;
     private String name;
     private String email;
     private String password;
     private String address;
+    private int age;
+    private Instant time;
 }
